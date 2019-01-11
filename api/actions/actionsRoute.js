0f5b96d59b1e7f55255da58da064b15c12dc7e54
ykrueng <yasirah.krueng@gmail.com>
2019-01-11 11:53:06 -0600

    fix GET /projects/:projectId/actions

--- api/actions/actionsRoute.js
+++ api/actions/actionsRoute.js
@@ -4,13 +4,8 @@ const actionDb = require("../../data/helpers/actionModel");
 
 const router = express.Router({ mergeParams: true });
 
-router.get("/", async (req, res, next) => {
-  try {
-    const actions = await actionDb.get();
-    res.status(200).json(actions);
-  } catch (err) {
-    next({ code: 500 });
-  }
+router.get("/", (req, res, next) => {
+  res.status(200).json(req.project.actions);
 });
 
 module.exports = router;
