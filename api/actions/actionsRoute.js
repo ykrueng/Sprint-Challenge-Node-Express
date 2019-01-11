a6fa252ec1c78a74b6238cf2b030d1cb0000d95b
ykrueng <yasirah.krueng@gmail.com>
2019-01-11 12:15:47 -0600

    add GET /projects/:projectId/actions/:actionId

--- api/actions/actionsRoute.js
+++ api/actions/actionsRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const actionDb = require("../../data/helpers/actionModel");
 const valActions = require("../common/valActions");
+const getAction = require("../common/getAction");
 
 const router = express.Router({ mergeParams: true });
 
@@ -12,7 +13,10 @@ router
   .post("/", valActions, async (req, res, next) => {
     const { projectId } = req.params;
     try {
-      const newAction = await actionDb.insert({project_id: projectId, ...req.action});
+      const newAction = await actionDb.insert({
+        project_id: projectId,
+        ...req.action
+      });
       if (newAction) {
         res.status(201).json(newAction);
       } else {
@@ -21,6 +25,9 @@ router
     } catch (err) {
       next({ code: 500 });
     }
+  })
+  .get("/:actionId", getAction, (req, res, next) => {
+    res.status(200).json(req.action);
   });
 
 module.exports = router;
