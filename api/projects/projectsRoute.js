a7687cf3eede1771acc76a947c4f86f9f6c80a6b
ykrueng <yasirah.krueng@gmail.com>
2019-01-11 11:25:19 -0600

    add DELETE /projects/:projectId

--- api/projects/projectsRoute.js
+++ api/projects/projectsRoute.js
@@ -27,8 +27,19 @@ router
       next({ code: 500 });
     }
   })
-  .get("/:projectId", getProject, async (req, res, next) => {
+  .get("/:projectId", getProject, (req, res, next) => {
     res.status(200).json(req.project);
   })
+  .delete("/:projectId", getProject, async (req, res, next) => {
+    const { projectId } = req.params;
+    try {
+      const deleted = await projectDb.remove(projectId);
+      if (deleted === 1) {
+        res.sendStatus(204);
+      }
+    } catch (err) {
+      next({ code: 500 });
+    }
+  });
 
 module.exports = router;
