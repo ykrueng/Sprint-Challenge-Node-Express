256d98f33da9dda863b8bb87cbc092fc372a305e
ykrueng <yasirah.krueng@gmail.com>
2019-01-11 11:10:10 -0600

    add POST /project

--- api/projects/projectsRoute.js
+++ api/projects/projectsRoute.js
@@ -1,16 +1,30 @@
 const express = require("express");
 
 const projectDb = require("../../data/helpers/projectModel");
+const valProject = require("../common/valProject");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const projects = await projectDb.get();
-    res.status(200).json(projects);
-  } catch (err) {
-    next({ code: 500 });
-  }
-});
+router
+  .get("/", async (req, res, next) => {
+    try {
+      const projects = await projectDb.get();
+      res.status(200).json(projects);
+    } catch (err) {
+      next({ code: 500 });
+    }
+  })
+  .post("/", valProject, async (req, res, next) => {
+    try {
+      const newProject = await projectDb.insert(req.project);
+      if (newProject) {
+        res.status(200).json(newProject);
+      } else {
+        next({ code: 500 });
+      }
+    } catch (err) {
+      next({ code: 500 });
+    }
+  });
 
 module.exports = router;
